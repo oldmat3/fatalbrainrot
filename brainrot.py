e801eef65036a600690ef3181c7eaa99366226e9
oldmat3 <132535460+oldmat3@users.noreply.github.com>
2024-08-02 21:06:36 +1000

    Add files via upload

--- brainrot.py
+++ brainrot.py
@@ -4,11 +4,17 @@ import edge_tts
 import os
 from pytube import YouTube
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from PIL import Image
+import io
 
 titleaudio = 'title.mp3'
 contentsaudio = 'contents.mp3'
+titless = 'element_screenshot.png'
 baskgroundvideo = '13 Minutes Minecraft Parkour Gameplay [Free to Use] [Map Download].mp4'
 video_url = 'https://www.youtube.com/watch?v=NX-i0IWl3yg'
+service = Service(r'C:\Users\kalen\Downloads\chromedriver_win32\chromedriver.exe')
 
 if os.path.exists(titleaudio):
     os.remove(titleaudio)
@@ -16,7 +22,9 @@ if os.path.exists(titleaudio):
 if os.path.exists(contentsaudio):
     os.remove(contentsaudio)
 
-#reddit scraping
+if os.path.exists(titless):
+    os.remove(titless)
+
 reddit = praw.Reddit(
     client_id="OAFOzx9mKDcw1hyTYc9h7A",
     client_secret="joLalAI15OzWENrytTxfJ9VWI07LTw",
@@ -31,10 +39,9 @@ top_post = next(subreddit.top(time_filter='day', limit=1))
 
 Title = top_post.title
 Contents = top_post.selftext
-print("Title retreived")
-print("Contents retreived")
+print("Title retrieved")
+print("Contents retrieved")
 
-#tts of title and contents of the post
 VOICES = ['en-US-GuyNeural', 'en-US-JennyNeural']
 TITLE_TEXT = Title
 CONTENTS_TEXT = Contents
@@ -49,14 +56,11 @@ async def save_speech(text, voice, output_file):
 
 async def amain():
     await save_speech(TITLE_TEXT, TITLE_VOICE, TITLE_OUTPUT_FILE)
-    
     await save_speech(CONTENTS_TEXT, CONTENTS_VOICE, CONTENTS_OUTPUT_FILE)
 
 if __name__ == "__main__":
     asyncio.run(amain())
 
-
-#gameplay download
 if os.path.exists(baskgroundvideo):
     print("background video already downloaded")
 else:
@@ -65,12 +69,23 @@ else:
     stream.download()
     print("Download completed")
 
+driver = webdriver.Chrome(service=service)
+driver.get('https://example.com')
 
-#screenshot of post title
-driver = webdriver.Firefox() 
-  
-driver.get("https://www.geeksforgeeks.org/") 
-  
-element = driver.find_element_by_class_name("header--navbar") 
-  
-element.screenshot('foo.png')
+element = driver.find_element(By.CSS_SELECTOR, 'h1')
+
+location = element.location
+size = element.size
+
+png = driver.get_screenshot_as_png()
+
+image = Image.open(io.BytesIO(png))
+left = location['x']
+top = location['y']
+right = location['x'] + size['width']
+bottom = location['y'] + size['height']
+
+image = image.crop((left, top, right, bottom))
+image.save('element_screenshot.png')
+
+driver.quit()
